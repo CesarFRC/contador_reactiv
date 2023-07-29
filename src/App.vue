86e50efd5f1d287f9bad2d124a76417cb34552f7
Cesar Reyes <cesar.reyes.cortinas12@gmail.com>
2023-07-29 14:15:28 -0600

    ejercicio terminado contador

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
-import {ref} from "vue";
+import {ref, computed} from "vue";
+
 const counter = ref(0);
+const numerofavorito = ref([]);
 const increment = () => {
   counter.value++;
 }
@@ -10,15 +12,41 @@ const decrement = () => {
 const reseta = () => {
   counter.value=0;
 }
+const añadirfavorito = () =>{
+  numerofavorito.value.push(counter.value); 
+}
+
+const classCounter = computed(() =>{
+  if (counter.value ===0 ){
+    return 'zero'
+  }
+  if (counter.value > 0 ){
+    return 'positivo'
+  }
+  if (counter.value < 0){
+    return 'negativo'
+  }
+ 
+})
+
 
 </script>
 
 <template>
   <h1> hola </h1>
-  <h2 :class="counter > 0 ? 'positivo' : 'negativo'"> {{ counter }}</h2>
+  <h2 :class="classCounter">{{ counter }}</h2>
  <button @click="increment"> alucoas</button>
  <button @click="decrement"> desencrimentar</button>
  <button @click="reseta"> reseteo</button>
+ <button @click="añadirfavorito"> añadir</button>
+ <ul>
+  <li v-for="number in numerofavorito" :key="number">
+{{ number }}
+  </li>
+ </ul>
+
+
+ 
 </template>
 
 <style>
@@ -28,5 +56,7 @@ const reseta = () => {
 .negativo{
   color: red
 }
-
+.zero{
+  color: peru
+}
 </style>
